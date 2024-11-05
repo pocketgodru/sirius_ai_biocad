8a6e52372e15af11a6bc2114d7aa6906998ab82e
pocketgodru <126785140+pocketgodru@users.noreply.github.com>
2024-11-05 12:03:14 +0400

    Update file_to_text.py

--- utils/file_to_text.py
+++ utils/file_to_text.py
@@ -1,12 +1,11 @@
 ﻿import os
-import fitz  # Correct import for pymupdf
+import fitz  
 from docx import Document
 from PIL import Image
 import mimetypes
 import io
 import base64
 
-# Функция для кодирования изображения в base64 без сохранения на диск
 def encode_image_bytes(image_bytes):
     return base64.b64encode(image_bytes).decode('utf-8')
 
@@ -23,7 +22,6 @@ def process_file(file_path):
         print(f"Unsupported file type: {mime_type}")
         return None, []
 
-# Загрузка текста и изображений из различных типов документов
 def load_text(file_name: str):
     ext = os.path.splitext(file_name)[1].lower()
     
@@ -41,25 +39,21 @@ def load_text(file_name: str):
         print(f"Error loading {file_name}: {e}")
         return None, []
 
-# Обработка PDF файлов
 def process_pdf(file_path):
     text = ""
     images = []
     pdf_document = fitz.open(file_path)
 
-    # Извлечение текста
     for page_num in range(len(pdf_document)):
         text += pdf_document[page_num].get_text("text")
 
-    # Извлечение изображений
     for page_num in range(len(pdf_document)):
         for _, img in enumerate(pdf_document.get_page_images(page_num, full=True)):
-            xref = img[0]  # Получаем ссылку на изображение
-            base_image = pdf_document.extract_image(xref)  # Извлекаем изображение
-            image_bytes = base_image["image"]  # Изображение в виде байтов
-            image_ext = base_image["ext"]  # Расширение изображения (png, jpeg и т.д.)
+            xref = img[0]  
+            base_image = pdf_document.extract_image(xref)  
+            image_bytes = base_image["image"]  
+            image_ext = base_image["ext"]  
 
-            # Конвертация изображения в base64 без сохранения на диск
             base64_image = encode_image_bytes(image_bytes)
             image_data = f"data:image/{image_ext};base64,{base64_image}"
 
@@ -67,17 +61,14 @@ def process_pdf(file_path):
 
     return text, images
 
-# Обработка DOCX файлов
 def process_docx(file_path):
     doc = Document(file_path)
     text = ""
     images = []
 
-    # Извлечение текста
     for paragraph in doc.paragraphs:
         text += paragraph.text + "\n"
 
-    # Извлечение изображений
     for rel in doc.part.rels.values():
         if "image" in rel.target_ref:
             img_data = rel.target_part.blob
@@ -89,7 +80,6 @@ def process_docx(file_path):
 
     return text, images
 
-# Обработка TXT файлов
 def process_txt(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         text = file.read()
